8a13721dc4632b4965c51a85ebcb3eb9092ba076
usama imran <usama.imran@frizhub.com>
2023-08-16 21:48:00 +0500

    add mongoDB Atlas connection and created user and note schema

---
+++ models/Note.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+const noteSchema = new mongoose.Schema({
+  user: {
+    // this basically tells mongoose that the type of this entry of Schema is of another Schema inside mongoose that we will later pass to it using 'ref'
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+    // ref refers to the model created by the Schema of which type this document of specific schema is.
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    default: "Employee",
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+noteSchema.plugin(AutoIncrement, {
+  inc_feild: "ticket",
+  start_seq: 500,
+  id: "ticketNums",
+});
+
+module.exports = mongoose.model("Note", noteSchema);
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 const { logger } = require("./middleware/logger");
+const { logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
-const { error } = require("console");
-
+// const { error } = require("console");
+// connect database.
+connectDB();
+// using logger, when the code gets inside this logger module it logs the requests requested by the user.
 app.use(logger);
+// using express.json(), this is a JSON parser
 app.use(express.json());
+//using express.static for static files
 app.use("/", express.static(path.join(__dirname, "public")));
+// this is to evaluate the get request of root route "/"
 app.use("/", require("./routes/root"));
-
+//this is a 404 handler, if the request isnt dealtup with until now and there is no respone then this will get called.
 app.all("*", (req, res) => {
   res.status(404);
   // req.accepts means that what the browser wants/accepts based on the request by the user. it either be html json.
@@ -24,6 +33,17 @@ app.all("*", (req, res) => {
 });
 
 app.use(errorHandler);
-app.listen(PORT, () => {
-  console.log("Server Running on port : " + PORT);
+
+mongoose.connection.once("open", () => {
+  console.log("Connection to mongoDB is successfull");
+  app.listen(PORT, () => {
+    console.log("Server Running on port : " + PORT);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  logEvents(
+    `${err.no}\t${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrorLog.log"
+  );
 });
