b0f23970129f823facc18a6ae664cc0b07e7946e
usama imran <usama.imran@frizhub.com>
2023-09-12 00:30:55 +0500

    final changes

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -9,7 +9,7 @@ const errorHandler = (err, req, res) => {
 
   const status = res.statusCode ? res.statusCode : 500; // server error
   res.status(status);
-  res.json({ message: err.message });
+  res.json({ message: err.message, isError: true });
 };
 
 module.exports = errorHandler;
